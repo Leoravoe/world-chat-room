84cfd07d62fa3ea28fb42ad7c390057f1a665dc6
Preetam Sarkar <sarkarpreetam12@gmail.com>
2021-03-14 02:18:10 +0530

    started building

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
-
+import React from 'react';
 import './App.css';
+import Chatsection from './Chatsection';
 
 function App() {
   return (
     <div className="app">
-      
+      <h1>My whatsapp clone</h1>
+      <div className="app-body">
+        {/* Chat component */}
+        <Chatsection />
+      </div>
     </div>
   );
 }
---
+++ src/Chatsection.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+
+const Chatsection = () => {
+    return (
+        <> 
+        {/* <div>I am chat Section</div> */}
+        <div className="chatBody">
+            <p>
+                <span className="chatName">
+                    Preetam
+                </span>
+                This is my message.
+            </p>
+        </div>
+        </>
+    );
+}
+
+export default Chatsection;
