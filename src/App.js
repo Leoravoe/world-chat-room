906b3e36ec9310292fd4a8c47511dd741a665fcc
Preetam Sarkar <sarkarpreetam12@gmail.com>
2021-03-17 22:31:06 +0530

    pusher trigger

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
-import React from 'react';
+import React, {useEffect} from 'react';
+
 import './App.css';
 import Chatsection from './Chatsection';
 
 function App() {
+  useEffect(() =>{
+    const pusher = new Pusher('07caf53696a6d62697d4', {
+      cluster: 'mt1'
+    });
+
+    var channel = pusher.subscribe('my-channel');
+    channel.bind('my-event', function(data) {
+      alert(JSON.stringify(data));
+    });
+  })
   return (
     <div className="app">
       {/* <h1>My whatsapp clone</h1> */}
